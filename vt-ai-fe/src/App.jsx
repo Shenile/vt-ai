2ea9aed848186cadaa0161e0c0d24a4e7aff738e
Shenile A <shenile.a@thinkpalm.com>
2025-07-10 11:48:39 +0530

    intial commit

---
+++ vt-ai-fe/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+function App() {
+
+
+  return (
+    <div className='text-lg'>
+        this is the header tag, [demo change].
+    </div>
+  )
+}
+
+export default App
