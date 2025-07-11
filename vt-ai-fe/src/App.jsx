70df0a1724cc7f6a01c8457dd5d81eb0551c2939
Shenile A <shenile.a@thinkpalm.com>
2025-07-11 15:45:57 +0530

    added a paaragraph

--- vt-ai-fe/src/App.jsx
+++ vt-ai-fe/src/App.jsx
@@ -6,7 +6,8 @@ function App() {
     <div className='text-lg'>
         <h1>I am test page.</h1>
         <p>this is a new commit</p>
-        <p>this is a new commit 2</p>    
+        <p>this is a new commit 2</p> 
+        <p>this is a new commit 3</p>    
     </div>
   )
 }
