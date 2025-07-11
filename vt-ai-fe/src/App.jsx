58923eb9eaf1983f7321291195207d93c4855290
Shenile A <shenile.a@thinkpalm.com>
2025-07-12 00:37:44 +0530

    sample commit

--- vt-ai-fe/src/App.jsx
+++ vt-ai-fe/src/App.jsx
@@ -8,6 +8,7 @@ function App() {
         <p>this is a new commit</p>
         <p>this is a new commit 2</p> 
         <p>this is a new commit 3</p>   
+        <p>this is a new commit 4</p>
         <img src="https://raw.githubusercontent.com/Shenile/vt-ai/visual-baselines/baselines/70df0a1724cc7f6a01c8457dd5d81eb0551c2939/homepage.png" alt="UI Snapshot" />
 
     </div>
