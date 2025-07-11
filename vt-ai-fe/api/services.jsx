7b7b4d32b210a8d9536b544e35852b9b244416af
Shenile A <shenile.a@thinkpalm.com>
2025-07-11 16:31:06 +0530

    created api folder

---
+++ vt-ai-fe/api/services.jsx
@@ -0,0 +1,3 @@
+import "axios"
+
+BACKEND_URL="http://localhost:3000"
--- vt-ai-fe/src/App.jsx
+++ vt-ai-fe/src/App.jsx
@@ -7,7 +7,9 @@ function App() {
         <h1>I am test page.</h1>
         <p>this is a new commit</p>
         <p>this is a new commit 2</p> 
-        <p>this is a new commit 3</p>    
+        <p>this is a new commit 3</p>   
+        <img src="https://raw.githubusercontent.com/Shenile/vt-ai/visual-baselines/baselines/70df0a1724cc7f6a01c8457dd5d81eb0551c2939/homepage.png" alt="UI Snapshot" />
+
     </div>
   )
 }
